26b8c2ede9cb1a6fc5b9899d28d04a16296feb64
vasilyii <voltronistvv@gmail.com>
2019-01-01 02:55:33 +0500

    remove newyear counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,48 +24,6 @@
 
             showInfo(getHexNumber(hexup, hexdown));
         });
-
-        function timeLeft(endtime){
-          var t = Date.parse(endtime) - Date.parse(new Date());
-          var seconds = Math.floor( (t/1000) % 60 );
-          var minutes = Math.floor( (t/1000/60) % 60 );
-          var hours = Math.floor( (t/(1000*60*60)) % 24 );
-          var days = Math.floor( t/(1000*60*60*24) );
-          return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-          };
-        };
-
-        var today = new Date();
-        var deadline = 'January 1 ' + (today.getFullYear() + 1) + " 00:00:00";
-        if (today.getMonth() == 0 && today.getDate() == 1) {
-            deadline = 'January 1 ' + (today.getFullYear()) + " 00:00:00";
-        };
-
-        var setClock = function(newyear){
-            var timeinterval = setInterval(function(){
-                var t = timeLeft(newyear);
-                $('#days').text(t.days);
-                $('#hours').text(t.hours);
-                $('#mins').text(('0' + t.minutes).slice(-2));
-                $('#secs').text(('0' + t.seconds).slice(-2));
-                if(t.total<=0){
-                    clearInterval(timeinterval);
-                    var now = new Date();
-                    var yearStr = now.getFullYear().toString();
-                    $('#text').text('С Новым Годом !!!')
-                    $('#days').text(yearStr[0]);
-                    $('#hours').text(yearStr[1]);
-                    $('#mins').text(yearStr[2]);
-                    $('#secs').text(yearStr[3]);
-                }
-            },1000);
-        };
-        setClock(deadline);
     });
     function getHexNumber(hexup, hexdown) {
         return hexArr[magic(hexdown)][magic(hexup)];
